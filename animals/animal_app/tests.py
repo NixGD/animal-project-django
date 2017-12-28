5cfcfcfb224677529e5d869088b00c9189dedec1
NixGD <nixgdill@gmail.com>
2017-12-28 15:38:07 -0500

    Adds Shape Tests

--- animals/animal_app/tests.py
+++ animals/animal_app/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .shapeObjects import RectangularPrism, Cylinder
+from math import pi
+from decimal import Decimal
 
-# Create your tests here.
+l = 2
+h = 3
+w = 5
+r = 7
+
+measures = {
+    "length": l,
+    "height": h,
+    "width": w,
+    "radius": r
+}
+
+class ShapeCalculationTests(TestCase):
+
+    def sa_and_vol(self, shape, vol=None, sa=None):
+        self.assertAlmostEqual(shape.get_vol(measures), Decimal(vol))
+        self.assertAlmostEqual(shape.get_sa(measures),  Decimal(sa))
+
+    def testRectangularPrism(self):
+        self.sa_and_vol(RectangularPrism,
+            vol=l*w*h,
+            sa= 2*(l*w + w*h + h*l))
+
+    def testCylinder(self):
+        self.sa_and_vol(Cylinder,
+            vol=pi*r*r*h,
+            sa= 2*pi*r * h + pi*r*r*2
+            )
