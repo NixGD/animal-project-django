a95260a3f94cd09dda3b373445210b33395b613e
bgoldowsky <bgoldowsky@cast.org>
2018-01-10 21:28:50 -0500

    Autosubmit state form.

--- animals/animal_app/static/animal_app/functions.js
+++ animals/animal_app/static/animal_app/functions.js
@@ -7,5 +7,13 @@ $(document).ready(function() {
         console.log('key');
         $('#notes_submit').prop("disabled", false);
     });
+
+    $(document).on('change',
+        '.state-selector',
+        function(e) {
+        console.log('noticed change in ' + e.target);
+        e.target.closest('form').submit();
+    });
+
 });
 
